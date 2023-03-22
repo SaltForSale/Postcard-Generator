1c1ea7ff12b26f1c279459e1918d80934e09f7d6
SaltForSale <elee823@gmail.com>
2023-03-22 11:09:16 -0400

    Commit 3

--- script.js
+++ script.js
@@ -8,7 +8,12 @@ states.onchange = function () {
   //logging the file name of the selected item in the dropdown
   console.log("img/" + states.value + "@2x.jpg");
   //changing the image of the div with id=postcard to the file name logged previously
-  document
-    .getElementById("postcard")
-    .style.backgroundImage = "url(img/" + states.value + "@2x.jpg)";
+  document.getElementById("postcard").style.backgroundImage =
+    "url(img/" + states.value + "@2x.jpg)";
+  //changing the header inside the postcard div with the name of the option you chose.
+  document.getElementById("state-heading").innerHTML =
+    states.options[states.selectedIndex].text;
+  //changing the text inside the greeting id to a string concatenated with the name of the state chosen.
+    document.getElementById("greeting").innerHTML =
+    'Greetings from ' + states.options[states.selectedIndex].text + '!';
 };
